faea70b294be3f220eeb2b7e4d0e5fbffb0ee349
Job Doesburg <job.doesburg@gmail.com>
2021-02-12 17:48:33 +0100

    Test with django saml2 pro auth

--- website/sagexit/urls.py
+++ website/sagexit/urls.py
@@ -1,13 +1,13 @@
-import django_saml2_auth
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+
+import saml2_pro_auth.urls as saml_urls
 
 urlpatterns = [
     path("", RedirectView.as_view(url="/reservations")),
     path("reservations/", include("room_reservation.urls")),
     path("users/", include("users.urls")),
     path("admin/", admin.site.urls),
-    path("saml2_auth/", include("django_saml2_auth.urls")),
-    path("saml2_auth/login/", django_saml2_auth.views.signin),
+    path("sso/saml/", include(saml_urls, namespace="saml")),
 ]
