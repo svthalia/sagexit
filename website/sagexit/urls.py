64525a8d2d0ecb541c57d4434c5678da9ba92b9b
Job Doesburg <job.doesburg@gmail.com>
2021-04-23 16:09:03 +0200

    Add a user logout view (#23)

--- website/sagexit/urls.py
+++ website/sagexit/urls.py
@@ -1,10 +1,18 @@
-from django.contrib import admin
+from django.contrib import admin, auth
+from django.shortcuts import redirect
 from django.urls import path, include
 from django.views.generic import RedirectView
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect("/")
+
 
 urlpatterns = [
     path("", RedirectView.as_view(url="/reservations")),
     path("reservations/", include("room_reservation.urls")),
     path("admin/", admin.site.urls),
+    path("logout/", logout, name="logout"),
     path("sso/", include("sp.urls")),
 ]
